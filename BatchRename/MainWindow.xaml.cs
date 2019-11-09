6d7549ada3c5ebbceb1cca5c65cc4fd85f9eb128
HUNG <hunghung274@gmail.com>
2019-11-10 01:03:19 +0700

    fix2(

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -93,10 +93,13 @@ namespace BatchRename
         }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
         private void frm_menu_FormClosing(object sender, FormClosingEventArgs e)
         {
             System.Windows.Forms.Application.Exit();
         }
+=======
+>>>>>>> parent of 1c731a0... update 1
 =======
 >>>>>>> parent of 1c731a0... update 1
 
@@ -232,6 +235,7 @@ namespace BatchRename
             //{
             //    btnClickTimes.Replace_Button++;
 
+<<<<<<< HEAD
 
             //    var prototype1 = new ReplaceOperation()
             //    {
@@ -258,6 +262,8 @@ namespace BatchRename
             ReplaceControl replaceControl = new ReplaceControl();
             replaceControl.ShowDialog();
 =======
+=======
+>>>>>>> parent of 1c731a0... update 1
             ReplaceOption rOption = new ReplaceOption();
           
             if (btnClickTimes.Replace_Button % 2 == 0)
